c9e06066514d20da4ed9c4887b37e6cd3d9e1a4d
Marcos Rangel <mrangel@gmail.com>
2017-12-01 02:00:30 -0200

    Versao 2.0

--- src/App.js
+++ src/App.js
@@ -1,61 +1,123 @@
-import React, { Component } from 'react'
-import Bookshelf from './Bookshelf'
-import SearchBooks from './SearchBooks'
-import { Route } from 'react-router-dom'
-import './App.css'
-import * as BooksAPI from './BooksAPI'
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import escapeRegExp from 'escape-string-regexp';
+import {
+  getAll,
+  update,
+  search,
+} from "./utils/BooksAPI";
 
+import HomePage from "./Pages/HomePage";
+import SearchPage from "./Pages/SearchPage";
 
-class BooksApp extends Component {
+class App extends Component {
 
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    books: []
+  constructor(props) {
+    super(props);
+    this.state = { books: [], booksToSearch: [] };
+    this.onSearch = this.onSearch.bind(this);
+    this.updateBook = this.updateBook.bind(this);
   }
 
   componentDidMount() {
-    this.getBooks();
+    (
+      async () => {
+        let books;
+        books = await getAll();
+        this.setState({books});
+      }
+    )();
   }
 
   getBooks() {
-   BooksAPI.getAll().then( (books) =>
-      this.setState({ books })
-    ) 
+    (
+      async () =>{
+        let books;
+        books = await getAll();
+        this.setState({books});
+      }
+    )(); 
+    this.clearSearch();
   }
 
-  updateBook = (book,shelf) => {
-    BooksAPI.update(book,shelf).then( () => {
-      this.getBooks()
-    })
+  onSearch(query) {
+    const { books } = this.state;
+    
+    if (query) {
+      search(query, 20).then((booksQuery) => {
+
+        const match = new RegExp(escapeRegExp(query),"i");
+
+        let myBooks = [];
+
+        myBooks = books.filter( book => (book.title && match.test(book.title)) || (book.authors && match.test(book.authors.toString()))) ;
+
+        if (booksQuery.error)
+          this.setState({booksToSearch: myBooks});
+        else {
+          if (books === []) {
+            this.setState({booksToSearch: booksQuery});
+          } 
+          else {
+            let newBooks = booksQuery.filter(function(bookQuery) {
+              return !myBooks.some(function(myBook){
+                return bookQuery.id === myBook.id;
+              });
+            });
+
+            newBooks = newBooks.concat(myBooks);
+
+            this.setState({booksToSearch: newBooks});
+          }
+        }
+      })
+    } 
+    else {
+      this.setState({booksToSearch: []});
+    }
+  }
+
+  clearSearch() {
+    this.setState({booksToSearch: []});
+  }
+
+  updateBook(book,shelf) {
+    (
+      async() => {
+          await update(book,shelf).then ( () => {
+            this.getBooks();
+          }).catch((err) => {
+              console.log(err);
+            })
+      }
+    )()
   }
 
   render() {
     return (
-      <div className="app">
-        <Route exact path="/" render={() => (
-          <Bookshelf
+        <div>
+          <Route exact path="/" render={() => (
+            <HomePage
               books={this.state.books} 
               onUpdateBook={this.updateBook}
-          />
-        )}/>
-        <Route path="/search" render={({history}) => (
-          <SearchBooks
-              books={this.state.books} 
-              onUpdateBook={(book,shelf) => {
-                this.updateBook(book,shelf)
-                history.push('/')
-              }}
-          />
-        )}/>
-      </div>
+            />
+          )}/>
+          <Route 
+            path="/search"
+            render={({history}) => (
+              <SearchPage
+                books={this.state.booksToSearch} 
+                onUpdateBook={(book,shelf) => {
+                  this.updateBook(book,shelf)
+                  history.push('/')
+                }}
+                onSearch={this.onSearch}
+                clearSearch={this.clearSearch}
+              />
+            )}/>
+        </div>
     )
   }
 }
 
-export default BooksApp;
-
+export default App;
